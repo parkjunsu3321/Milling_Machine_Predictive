e5aa8945fc312c17e697438c36693ba14519f1ce
parkjunsu3321 <jackpot3016@naver.com>
2026-09-20 12:50:56 +0900

    "feat: LGBM 모델 학습 추가"

---
+++ train/model/fix/lgbm_fix.py
@@ -0,0 +1,16 @@
+from lightgbm import LGBMClassifier
+from sklearn.model_selection import train_test_split
+from ..data_loader import data_load
+from ..eval.eval_metric import print_metrics
+
+df = data_load()
+
+X = df.drop(columns=['기계고장'])
+y = df['기계고장']
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=1)
+
+model = LGBMClassifier(n_estimators=200,learning_rate=0.2,max_depth=1)
+model.fit(X_train,y_train)
+pred_test = model.predict(X_test)
+
+print_metrics(y_test, pred_test, "LGBM 결과")
