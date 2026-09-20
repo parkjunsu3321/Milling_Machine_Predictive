5eb17b3f2458e16f126b1a1efd7a75f2917e4cba
parkjunsu3321 <jackpot3016@naver.com>
2026-09-20 23:16:21 +0900

    "feat: Catboost 모델 학습 코드"

--- train/model/fit/lgbm_fit.py
+++ train/model/fit/lgbm_fit.py
@@ -1,5 +1,5 @@
+from lightgbm import LGBMClassifier
 from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
 from ..data_loader import data_load
 from ..eval.eval_metric import print_metrics
 
@@ -9,8 +9,13 @@ X = df.drop(columns=['기계고장'])
 y = df['기계고장']
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=1)
 
-model = XGBClassifier(n_estimators=200,learning_rate=0.2,max_depth=1)
+model = LGBMClassifier(n_estimators=200,learning_rate=0.2,max_depth=1,random_state=42)
 model.fit(X_train,y_train)
 pred_test = model.predict(X_test)
 
-print_metrics(y_test, pred_test, "XGBoost 결과")
+
+print("---------------------------------")
+print("---------------------------------")
+print_metrics(y_test, pred_test, "LGBM 결과")
+print("---------------------------------")
+print("---------------------------------")
